0af11c73f6c4f11987597cbaefad957122bfc4d9
Amin Bagheri <amin.bagheri@gmail.com>
2022-10-22 14:25:26 -0400

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Amin Bagheri on 2022-10-22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
