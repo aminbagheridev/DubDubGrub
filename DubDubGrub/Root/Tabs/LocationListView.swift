b2b83793d5505b5a03ef55cd1c476569b1b99338
Amin Bagheri <amin.bagheri@gmail.com>
2022-10-22 15:33:23 -0400

    Updated Location list view with dummy UI to create a list of locations with an image, title and list of avatars

--- DubDubGrub/Root/Tabs/LocationListView.swift
+++ DubDubGrub/Root/Tabs/LocationListView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("Location List")
+        NavigationView {
+            List {
+                ForEach(0..<10) { item in
+                    HStack {
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+                        VStack(alignment: .leading) {
+                            Text("Test Location Name")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            HStack {
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                            }
+                        }
+                        .padding(.leading, 10)
+                    }
+                }
+            }
+            .navigationTitle("Grub Spots")
+        }
     }
 }
 
@@ -18,3 +47,5 @@ struct LocationListView_Previews: PreviewProvider {
         LocationListView()
     }
 }
+
+
